15437d9e31cb87f974fea0d7cb40e07203f13ea3
Ayush Singh <125784866+AyushError@users.noreply.github.com>
2025-05-11 18:54:53 +0530

    Create types.ts

---
+++ lib/types.ts
@@ -0,0 +1,8 @@
+export enum CardType {
+  POKEMON = "Pokémon",
+  TRAINER = "Trainer",
+  ENERGY = "Energy"
+}
+
+export type PokemonType = "Fire" | "Water" | "Electric" | "Poison" | "Fashion" | "Sound" | "Smoke"
+export type TrainerSubtype = "Supporter" | "Stadium" | "Item"
