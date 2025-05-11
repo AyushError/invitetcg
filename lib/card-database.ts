a72df38ddbda1e9e5de0bdade1869f226703c2bd
Ayush Singh <125784866+AyushError@users.noreply.github.com>
2025-05-11 18:01:15 +0530

    Update card-database.ts

--- lib/card-database.ts
+++ lib/card-database.ts
@@ -5,20 +5,17 @@ export function getStarterDeck() {
   return [
     // Pokémon Cards
     {
-      id: "pika1",
+      id: "sutta",
       type: CardType.POKEMON,
       name: "sutta",
       hp: 60,
       stage: "Basic",
-      pokemonType: PokemonType.LIGHTNING,
-      weakness: PokemonType.FIGHTING,
       resistance: null,
       retreatCost: 1,
       attacks: [
         {
           name: "puff",
           damage: 10,
-          energyCost: [PokemonType.LIGHTNING],
           effect: "coughing",
         },
       ],
@@ -26,76 +23,73 @@ export function getStarterDeck() {
     {
       id: "rai1",
       type: CardType.POKEMON,
-      name: "Raichu",
+      name: "Invite",
       hp: 90,
-      stage: "Stage 1",
-      evolvesFrom: "Pikachu",
-      pokemonType: PokemonType.LIGHTNING,
-      weakness: PokemonType.FIGHTING,
+      stage: "basic",
       resistance: null,
       retreatCost: 1,
       attacks: [
         {
-          name: "Agility",
+          name: "location",
           damage: 20,
-          energyCost: [PokemonType.LIGHTNING, PokemonType.COLORLESS],
           effect:
-            "Flip a coin. If heads, during your opponent's next turn, prevent all effects of attacks, including damage, done to this Pokémon.",
-        },
-        {
-          name: "Thunder",
-          damage: 60,
-          energyCost: [PokemonType.LIGHTNING, PokemonType.LIGHTNING, PokemonType.COLORLESS],
-          effect: "This Pokémon does 10 damage to itself.",
+            "u go to kyoto and say my name",
         },
       ],
     },
     {
       id: "bulb1",
       type: CardType.POKEMON,
-      name: "Bulbasaur",
+      name: "cocktail",
       hp: 60,
       stage: "Basic",
-      pokemonType: PokemonType.GRASS,
-      weakness: PokemonType.FIRE,
       resistance: null,
       retreatCost: 1,
       attacks: [
         {
-          name: "Leech Seed",
+          name: "nausea",
           damage: 20,
-          energyCost: [PokemonType.GRASS, PokemonType.COLORLESS],
-          effect: "Remove 1 damage counter from this Pokémon.",
+          effect: "gives u nausea effect.",
         },
       ],
     },
     {
       id: "ivy1",
       type: CardType.POKEMON,
-      name: "Ivysaur",
+      name: "dresscode",
       hp: 70,
-      stage: "Stage 1",
-      evolvesFrom: "Bulbasaur",
-      pokemonType: PokemonType.GRASS,
-      weakness: PokemonType.FIRE,
+      stage: "basic",
       resistance: null,
       retreatCost: 1,
       attacks: [
         {
-          name: "Vine Whip",
+          name: "your drip",
           damage: 30,
-          energyCost: [PokemonType.GRASS, PokemonType.COLORLESS],
           effect: null,
         },
-        {
-          name: "Poisonpowder",
-          damage: 20,
-          energyCost: [PokemonType.GRASS, PokemonType.GRASS, PokemonType.COLORLESS],
-          effect: "The Defending Pokémon is now Poisoned.",
-        },
+     
   
       ],
     },
+     {
+      id: "ivy1",
+      type: CardType.POKEMON,
+      name: "kareoke",
+      hp: 70,
+      stage: "basic",
+      resistance: null,
+      retreatCost: 1,
+      attacks: [
+        {
+          name: "do your song",
+          damage: 30,
+          effect: null,
+        },
+     
+  
+      ],
+    },
+
 
 
   ]
